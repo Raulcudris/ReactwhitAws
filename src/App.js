fdef23a373251dc028abc72954ad97c596a2049f
RaulCudris <raulcudriz@hotmail.com>
2023-08-02 19:31:40 -0500

    Actualizacion del Proyecto amplify con aws

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { API, graphqlOperation } from 'aws-amplify';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { createTask } from './graphql/mutations';
+import { listTasks } from './graphql/queries';
 
 function App() {
+  
+  const [task , setTask] = useState({
+    name: "",
+    description:""
+  })
+
+  const [tasks , setTasks] = useState([]);
+
+  const handleSubmit  = async (e)=>{
+      e.preventDefault();
+      //console.log(task);
+      const result = await API.graphql(graphqlOperation(createTask, {input: task}))
+      console.log(result);
+  }
+
+  useEffect(()=>{
+    async function loadTasks() {
+      const result = await API.graphql(graphqlOperation(listTasks))
+      console.log(result)
+      setTasks(result.data.listTasks.items);
+    }
+    loadTasks()
+  },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <form onSubmit={handleSubmit} >
+      <input  name='name' 
+              placeholder='Title'
+              onChange={e => setTask({
+                ...task, name: e.target.value
+              })}
+              />
+      <textarea name='description'
+                placeholder='description'
+                onChange={e => setTask({
+                  ...task, description: e.target.value
+                })}
+                /> 
+      <button >Submit</button>
+    </form>
+    { JSON.stringify(tasks)}
+    //41:06 https://www.youtube.com/watch?v=ZBMYWnQ-_EM
+    </>  
+    );
 }
 
 export default App;
